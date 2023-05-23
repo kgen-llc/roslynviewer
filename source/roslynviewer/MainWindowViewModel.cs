6bc24841e7466847734d536303c6e71d34b6dade
Frederic Forjan <fforjan@free.fr>
2023-05-23 06:28:14 -0700

    Provide App Title in VM

--- source/roslynviewer/MainWindowViewModel.cs
+++ source/roslynviewer/MainWindowViewModel.cs
@@ -5,6 +5,7 @@ using Avalonia.Controls.ApplicationLifetimes;
 using System.Collections.Generic;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Microsoft.CodeAnalysis.CSharp;
+using System.Reflection;
 
 public class MainWindowViewModel  : ObservableObject 
 {
@@ -12,9 +13,19 @@ public class MainWindowViewModel  : ObservableObject
         this.controlledApplicationLifetime = controlledApplicationLifetime;
         this.sourceCode = string.Empty;
         this.syntaxTreeRoot = new List<SyntaxNodeViewModel> ();
+
+        this.ApplicationTitle = getTitle();
+
+        string getTitle()
+        {
+            var assembly= Assembly.GetEntryAssembly()!;
+
+            return assembly.GetCustomAttribute<AssemblyProductAttribute>()!.Product + " " + assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion.ToString()
+                + " - " + assembly.GetCustomAttribute<AssemblyCopyrightAttribute>()!.Copyright;
+        }
     }
     private string sourceCode;
-    private  List<SyntaxNodeViewModel> syntaxTreeRoot;
+    private IReadOnlyCollection<SyntaxNodeViewModel> syntaxTreeRoot;
     private readonly IControlledApplicationLifetime controlledApplicationLifetime;
 
     public string SourceCode {
@@ -29,10 +40,12 @@ public class MainWindowViewModel  : ObservableObject
         }
     }
 
-    public List<SyntaxNodeViewModel> SyntaxTreeRoot {
+    public IReadOnlyCollection<SyntaxNodeViewModel> SyntaxTreeRoot {
         get => this.syntaxTreeRoot;
         set => this.SetProperty(ref this.syntaxTreeRoot, value);
     }
+
+    public string ApplicationTitle { get; } 
 
     public void ExitCommand() {
         this.controlledApplicationLifetime.Shutdown(0);
