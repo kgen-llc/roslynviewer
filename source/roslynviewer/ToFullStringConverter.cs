c1ae6edecef43dc5b829b7e950a1319fef63d592
Frederic Forjan <fforjan@free.fr>
2023-05-23 06:30:45 -0700

    implement convertback properly

--- source/roslynviewer/ToFullStringConverter.cs
+++ source/roslynviewer/ToFullStringConverter.cs
@@ -24,6 +24,6 @@ public class ToFullStringConverter : IValueConverter
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        throw new NotSupportedException();
+        return new BindingNotification(new NotSupportedException(), BindingErrorType.Error);
     }
 }
